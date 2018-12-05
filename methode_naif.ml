1f89036caca495abdb5e16108dfca29ed2448c93
Charlie Meyer <meyercharl@eisti.eu>
2018-12-05 12:00:34 +0100

    Implementation du calcul de la premiere proposition

--- methode_naif.ml
+++ methode_naif.ml
@@ -5,5 +5,12 @@ module Naif :
      sig
      end=
      struct
+     (** Calcule aleatoirement la premiere proposition a donner*)
+     let per_p =
+          let possibles = Code.tous in
+               let taille = List.length possibles in
+                    List.nth (Random.int tailles) possibles;; 
+
+
 
 end;;
