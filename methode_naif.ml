68201a70b604a5a54a5d49194f90b66ce5039a7b
Charlie Meyer <meyercharl@eisti.eu>
2018-12-06 13:29:54 +0100

    Implementation du debut de la methode naïve

--- methode_naif.ml
+++ methode_naif.ml
@@ -1,16 +1,37 @@
 (** Algorithmes de la methode naif *)
-#use "code.ml"
+#use "code.ml";;
 
 module Naif :
      sig
+     (** Calcule aleatoirement la premiere proposition a donner*)
+     val per_p : Code.t
+
+     (** Supprime un element de la liste
+       * @param liste liste des codes courants
+       * @param element element de la liste courante à supprimer
+       * @return la liste sans l'element à supprimer
+       *)
+     val supprime : Code.t list -> Code.t -> Code.t list
+
+     (**Donne la liste des codes courants (sans filtrage) après la première proposition*)
+     val liste_perp : Code.t list
+
      end=
      struct
-     (** Calcule aleatoirement la premiere proposition a donner*)
+
      let per_p =
           let possibles = Code.tous in
-               let taille = List.length possibles in
-                    List.nth (Random.int tailles) possibles;; 
+               let tailles = List.length possibles in
+                    List.nth possibles (Random.int tailles);;
 
+     let rec supprimeRT liste element acc=
+          match (liste) with
+               | []  ->  acc
+               | (n :: suite) when (List.for_all2 (fun x y -> x=y) n element) -> supprimeRT suite element acc
+               | (n :: suite)                                                 -> supprimeRT suite element (acc @ [n]);;
 
+     let supprime liste element = supprimeRT liste element [];;
+
+     let liste_perp = supprime (Code.tous) per_p;;
 
 end;;
