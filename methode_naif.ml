0a9ee1458ea77a42aa037cf544ad8e2776924117
Charlie Meyer <meyercharl@eisti.eu>
2018-12-06 14:37:46 +0100

    Implementation des differentes fonctions de suppression

--- methode_naif.ml
+++ methode_naif.ml
@@ -3,35 +3,62 @@
 
 module Naif :
      sig
-     (** Calcule aleatoirement la premiere proposition a donner*)
-     val per_p : Code.t
+     (** Calcule aleatoirement une proposition a donner
+       * @param possible la liste courante des codes possibles
+       * @return un code aleatoire parmis tous les codes possibles
+       *)
+     val per_p : Code.t list -> Code.t
 
      (** Supprime un element de la liste
        * @param liste liste des codes courants
        * @param element element de la liste courante à supprimer
        * @return la liste sans l'element à supprimer
        *)
-     val supprime : Code.t list -> Code.t -> Code.t list
+     val supprime_element : Code.t list -> Code.t -> Code.t list
 
-     (**Donne la liste des codes courants (sans filtrage) après la première proposition*)
-     val liste_perp : Code.t list
+     (** Supprime des elements de la liste en accord avec la reponse obtenue
+       * @param liste liste des codes courants
+       * @param element element de la liste courante dont au moins un pion est "valide"
+       * @return la liste sans les elements à supprimer
+       *)
+     val supprime_couleur : Code.t list -> Code.t -> Code.t list
+
+     (** Supprime des elements de la liste en accord avec la reponse obtenue
+       * @param liste liste des codes courants
+       * @param element element de la liste courante dont les 4 pions sont "valide"
+       * @return la liste sans les elements à supprimer
+       *)
+     val supprime_code4 : Code.t list -> Code.t -> Code.t list
 
      end=
      struct
 
-     let per_p =
-          let possibles = Code.tous in
-               let tailles = List.length possibles in
-                    List.nth possibles (Random.int tailles);;
+     let per_p possible=
+          let tailles = List.length possibles in
+               List.nth possibles (Random.int tailles);;
 
-     let rec supprimeRT liste element acc=
+     let rec supprime_elementRT liste element acc=
           match (liste) with
                | []  ->  acc
-               | (n :: suite) when (List.for_all2 (fun x y -> x=y) n element) -> supprimeRT suite element acc
-               | (n :: suite)                                                 -> supprimeRT suite element (acc @ [n]);;
+               | (n :: suite) when (List.for_all2 (fun x y -> x=y) n element) -> supprime_elementRT suite element acc
+               | (n :: suite)                                                 -> supprime_elementRT suite element (acc @ [n]);;
 
-     let supprime liste element = supprimeRT liste element [];;
+     let supprime_element liste element = supprime_elementRT liste element [];;
 
-     let liste_perp = supprime (Code.tous) per_p;;
+     let rec supprime_couleurRT liste element acc=
+          match (liste) with
+               | [] -> acc
+               | (n :: suite) when (List.exists2 (fun x y -> x=y) n element) -> supprime_couleurRT suite element (acc @ [n])
+               | (n :: suite)                                                -> supprime_couleurRT suite element acc;;
+
+     let supprime_couleur liste element = supprime_couleurRT liste element [];;
+
+     let rec supprime_code4RT liste element acc =
+          match (liste) with
+               | [] -> acc
+               | (n :: suite) when List.for_all (fun x -> List.exists (fun y -> x=y) element) n -> supprime_code4RT suite element (acc @ [n])
+               | (n :: suite)                                                                   -> supprime_code4RT suite element acc;;
+
+     let supprime_code4 liste element = supprime_code4RT liste element [];;
 
 end;;
