c594c2048fb1c81271ed6040f6064fd01cc35f4c
Charlie Meyer <meyercharl@eisti.eu>
2018-12-12 11:55:33 +0100

    Implementation de la fonction supprime_code4

--- methode_naif.ml
+++ methode_naif.ml
@@ -33,7 +33,7 @@ module Naif :
      end=
      struct
 
-     let per_p possible=
+     let per_p possibles=
           let tailles = List.length possibles in
                List.nth possibles (Random.int tailles);;
 
@@ -53,12 +53,48 @@ module Naif :
 
      let supprime_couleur liste element = supprime_couleurRT liste element [];;
 
-     let rec supprime_code4RT liste element acc =
+     let rec search acc2 liste element acc =
           match (liste) with
                | [] -> acc
-               | (n :: suite) when List.for_all (fun x -> List.exists (fun y -> x=y) element) n -> supprime_code4RT suite element (acc @ [n])
-               | (n :: suite)                                                                   -> supprime_code4RT suite element acc;;
+               | (n :: suite) when (n = element) -> acc
+               | (n :: suite)                    -> search acc2 suite element (acc+1);;
 
-     let supprime_code4 liste element = supprime_code4RT liste element [];;
+     let rec modifie liste nth acc res=
+          match (liste) with
+               | [] -> res
+               | (n :: suite) when (n=1)       -> modifie suite nth (acc+1) (res @ [1])
+               | (n :: suite) when (acc = nth) -> modifie suite nth (acc+1) (res @ [1])
+               | (n :: suite)                  -> modifie suite nth (acc+1) (res @ [0]);;
+
+     let rec creer_liste_0 acc liste=
+          if (acc=Code.nombre_pions) then
+               liste
+          else
+               creer_liste_0 (acc+1) (liste @ [0]);;
+
+     let rec verif2RT mp n etat acc =
+          match (n) with
+               | [] -> (false,etat)
+               | (np :: suite) -> (if ((mp=np)&& (List.nth etat acc)=0) then
+                                        let etat = modifie etat acc 0 [] in
+                                             (true,etat)
+                                   else
+                                        verif2RT mp suite etat (acc+1));;
+
+     let rec verifRT element n etat acc=
+          match (element) with
+               | [] -> acc
+               | (mp :: suite) -> (let (valeur,etatp) = verif2RT mp n etat 0 in
+                                        if valeur then
+                                             let etat = etatp in
+                                                  verifRT suite n etat acc
+                                        else
+                                             false);;
+
+     let verif element n =
+          let acc2 = creer_liste_0 0 [] in
+               verifRT element n acc2 true ;;
+
+     let supprime_code4 liste element = let res = List.filter (fun x -> verif element x) liste in supprime_element res element;;
 
 end;;
